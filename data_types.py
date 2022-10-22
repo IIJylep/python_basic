64c517ae9c740acafda5e2ba21dabc26557294ac
Yura <iamchuk.iura@gmail.com>
2022-10-22 14:16:49 +0300

    add data types file

---
+++ data_types.py
@@ -0,0 +1,23 @@
+online_course = "Python"
+mentor_name = "Artem"
+trade_mark = "Grow"
+business_description = "Marketing agency"
+company_budget = 10
+company_profit = 15
+traffic_site = 5
+advertising_expense = 8
+type_advertising = "PPC"
+number_conversion = 2
+
+print(
+    online_course, type(online_course),
+    mentor_name, type(mentor_name),
+    trade_mark, type(trade_mark),
+    business_description, type(business_description),
+    company_budget, type(company_budget),
+    company_profit, type(company_profit),
+    traffic_site, type(traffic_site),
+    advertising_expense, type(advertising_expense),
+    type_advertising, type(type_advertising),
+    number_conversion, type(number_conversion)
+)
