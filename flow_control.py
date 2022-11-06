9b949954a05406c64af59d3a599b3998f8506f0a
Yura <iamchuk.iura@gmail.com>
2022-11-06 20:08:34 +0200

    add loops file

--- flow_control.py
+++ flow_control.py
@@ -2,19 +2,14 @@ first_data = str(input(f"Enter the first operand: "))
 second_data = str(input(f"Enter the second operand: "))
 operation = str(input(f"Enter an operation: "))
 
-if '.' in first_data and '.' in second_data:
+if '.' in first_data:
     operand_one = float(first_data)
-    operand_two = float(second_data)
-
-elif '.' in first_data:
-    operand_one = float(first_data)
-    operand_two = int(second_data)
-
-elif '.' in second_data:
-    operand_one = int(first_data)
-    operand_two = float(second_data)
 else:
     operand_one = int(first_data)
+
+if '.' in second_data:
+    operand_two = float(second_data)
+else:
     operand_two = int(second_data)
 
 if operation == '+':
